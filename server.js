8cb4f3c34e8246d6d0b9cf92939404c0add7fe45
George O. E. Campbell <campbell.goe@gmail.com>
2019-03-01 06:28:12 +0000

    functional contact form

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const next = require('next')
-
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
-
+const bodyParser = require('body-parser');
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+let emails = [];
 app.prepare()
 .then(() => {
-  const server = express()
-
+  const server = express();
+  server.use(bodyParser.json());
   server.get('*', (req, res) => {
     return handle(req, res)
   })
