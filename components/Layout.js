0b4782851e7d80ea196b579e16e5ee39e7ad4271
George O. E. Campbell <campbell.goe@gmail.com>
2019-03-01 10:34:09 +0000

    minor

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,4 @@
 import Header from './Header';
-import Head from 'next/head';
 import Link from 'next/link';
 import { Component } from 'react';
 const bottomColour = '#ffb1f9';
@@ -45,12 +44,6 @@ class Layout extends Component {
   render(){
     return (
       <React.Fragment>
-        <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta charSet="utf-8" />
-          <meta name="theme-color" content="#c6b3fb"/>
-          <title>TaskTimer - make timers for tasks</title>
-        </Head>
         <section>
           {this.state.nightModeOn ? <i
             className="material-icons night-mode-icon light-mode-icon"
@@ -83,41 +76,6 @@ class Layout extends Component {
         </section>
         <style jsx global>{`
           @import url('https://fonts.googleapis.com/css?family=Hind+Guntur|Josefin+Sans:700');
-          @font-face {
-            font-family: 'Material Icons';
-            font-style: normal;
-            font-weight: 400;
-            src: url(../static/fonts/MaterialIcons-Regular.eot); /* For IE6-8 */
-            src: local('Material Icons'),
-              local('MaterialIcons-Regular'),
-              url(../static/fonts/MaterialIcons-Regular.woff2) format('woff2'),
-              url(../static/fonts/MaterialIcons-Regular.woff) format('woff'),
-              url(../static/fonts/MaterialIcons-Regular.ttf) format('truetype');
-          }
-          .material-icons {
-            font-family: 'Material Icons';
-            font-weight: normal;
-            font-style: normal;
-            font-size: 24px;  /* Preferred icon size */
-            display: inline-block;
-            line-height: 1;
-            text-transform: none;
-            letter-spacing: normal;
-            word-wrap: normal;
-            white-space: nowrap;
-            direction: ltr;
-
-            /* Support for all WebKit browsers. */
-            -webkit-font-smoothing: antialiased;
-            /* Support for Safari and Chrome. */
-            text-rendering: optimizeLegibility;
-
-            /* Support for Firefox. */
-            -moz-osx-font-smoothing: grayscale;
-
-            /* Support for IE. */
-            font-feature-settings: 'liga';
-          }
 
           html {
             background: linear-gradient(108deg,${this.state.bottomColour} 0%,${this.state.topColour} 100%);
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import App, { Container } from 'next/app';
 import Layout from '../components/Layout.js'
-
+import Head from 'next/head';
 class MyApp extends App {
   render () {
     const { Component, pageProps } = this.props
 
     return (
       <Container>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta charSet="utf-8" />
+          <meta name="theme-color" content="#c6b3fb"/>
+          <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet"/>
+          <title>TaskTimer - make timers for tasks</title>
+        </Head>
         <Layout>
           <Component {...pageProps} />
         </Layout>
