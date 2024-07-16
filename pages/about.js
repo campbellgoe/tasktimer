905591ba8800f120fbc4a6ae09ee9fd7f1c17e02
George Campbell <campbell.goe@gmail.com>
2024-07-16 13:35:44 +0100

    update about

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,14 @@
 export default () => (
     <div className="about-paragraphs about">
+      {/* p>When you're done with a task, click the stop button to save your time.</p>
+      <p>To view your total time spent on each task, click on the task name.</p>
+      <p>To delete a task, click on the delete button next to the task name.</p>
+      <p>To export your data, click the export button in the top right corner.</p>
+      <p>To import your data, click the import button */}
       <p>This app is for designers, freelancers, and other workers who want to track how much time they spend on their work.</p>
       <p>Make a timer for each task in your project, start and stop the timers as you work on each task.</p>
       <p>The stripe at the bottom of each task represents a 25 minute timer.</p>
+      <p>This tool was built in 2020 by George. Contact me via my website if you are interested in hiring me: <a href="https://massless.ltd">https://massless.ltd</a></p>
       <style jsx>{`
         p:last-child {
           margin-bottom: 0;
